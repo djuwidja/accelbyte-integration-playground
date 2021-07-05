d41d6b095d8a23249aba4efa36aecbea1fc37beb
kenneth <kenneth@accelbyte.net>
2021-07-05 16:31:12 -0400

    Login flow successful with AccelByte. Added Data Stash to manage persistent data in the game lifecycle such as the user profile.

---
+++ Assets/Scripts/DataStash.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class DataStash : MonoBehaviour
+{
+    private AccelByte.Api.User AccelByteUser;
+
+    //Lazy singleton
+    private static DataStash Instance;
+    public static DataStash GetInstance()
+    {
+        return Instance;
+    }
+
+    //Persistent data store across the game
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        Instance = this;
+    }
+
+    void Start()
+    {
+        AccelByteUser = AccelByte.Api.AccelBytePlugin.GetUser();
+    }
+
+    //Cleanup
+    void OnDestroy()
+    {
+        Instance = null;
+    }
+
+    public AccelByte.Api.User GetAccelByteUser()
+    {
+        return AccelByteUser;
+    }
+}
---
+++ Assets/Scripts/LoginScene/LoginSceneController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginSceneController : MonoBehaviour
+{
+    public InputField UserNameInput;
+    public InputField PasswordInput;
+    public Button LoginButton;
+
+    private DataStash dataStash;
+
+    void Start()
+    {
+        //initialize
+        LoginButton.onClick.AddListener(OnLoginBtnClicked);
+        dataStash = DataStash.GetInstance();  
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnLoginBtnClicked()
+    {
+         if (UserNameInput.text != "" && PasswordInput.text != "")
+         {
+            LoginButton.enabled = false;
+
+            Debug.Log(string.Format("Loggging in to AccelByte with user name: {0} and password: {1}", UserNameInput.text, PasswordInput.text));
+            var accelByteUser = dataStash.GetAccelByteUser();
+            accelByteUser.LoginWithUsername(UserNameInput.text, PasswordInput.text,
+                result =>
+                {
+                    if (result.IsError)
+                    {
+                        Debug.Log("Login failed");
+                        LoginButton.enabled = true;
+                    }
+                    else
+                    {
+                        Debug.Log("Login successful");
+                        //switch scene
+                    }
+                });                
+        }
+    }
+}
